8b0b29df9a3a26b5dd2041ba181514821b54f1f2
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2021-11-23 11:25:01 +0530

    Update medic.sol

--- contracts/medic.sol
+++ contracts/medic.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract superadmin{
-    address admin;
+    address admin = 0x473c68D8eBE549F820d15BcE0A493e0d6990a38c;
     mapping(address => bool) public hosAdmin;
     
     struct Patient{
@@ -10,9 +10,10 @@ contract superadmin{
         string aadhar;
         string dob;
         string sex;
-        bytes32[] docs;
+        string[] docs;
     }
     mapping(address => Patient) public patients;
+    mapping(address => bool) public patientexist;
     address[] patList;
     
     struct Hospital{
@@ -31,6 +32,7 @@ contract superadmin{
     }
     
     function newPatient(string memory n,string memory a, string memory d, string memory s) public{
+        require(hosAdmin[msg.sender]==false);
         patients[msg.sender].name=n;
         patients[msg.sender].aadhar=a;
         patients[msg.sender].dob = d;
@@ -38,11 +40,27 @@ contract superadmin{
         patList.push(msg.sender);
     }
     
-    /*function upDoc(bytes32 hash) public{
+    function upDoc(address pAdd, string memory hash) public{
         require(hosAdmin[msg.sender]);
-    }*/
+        patients[pAdd].docs.push(hash);
+    }
     
     function getPatients() public view returns (address[] memory) {
         return patList;
+    }
+
+    function checkAdmin(address add) public view returns (bool) {
+        return hosAdmin[add];
+    }
+    function checkPatient(address add) public view returns (bool) {
+        return patientexist[add];
+    }
+
+    function getPatientDetails() public view returns (string memory,string memory, string memory, string memory, string[] memory) {
+        return (patients[msg.sender].name,
+            patients[msg.sender].aadhar,
+            patients[msg.sender].dob,
+            patients[msg.sender].sex,
+            patients[msg.sender].docs);
     }
 }
