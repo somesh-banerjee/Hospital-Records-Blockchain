ff181902fbb28ecb216fdc79bb3b72470c5e28a0
freakfan15 <vivekkrrai.bxr@gmail.com>
2021-11-25 01:59:06 +0530

    fixed fetching data from eth

--- frontend/src/screens/PatientScreen.js
+++ frontend/src/screens/PatientScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import patients from "../data/patients";
 import Nav from "../components/Nav";
@@ -6,12 +6,33 @@ import FileUpload from "../components/FileUpload";
 import contract from "../ethereum/superadmin";
 
 const PatientScreen = ({ match }) => {
-	const patient = patients.find((p) => p._id === match.params.id);
+	// const patient = patients.find((p) => p._id === match.params.id);
+	const [patient, getPatient] = useState({
+		address: "",
+		name: "",
+		aadhar: "",
+		dob: "",
+		sex: "",
+		docs: [],
+		docsD: []
+	})
 	
 	const summary = async()=>{
 		const sum = await contract.methods.getPatientDetails(match.params.id).call();
-		console.log(sum);
-	} 
+		getPatient(
+			{
+				address: match.params.id,
+				name: sum[0],
+				aadhar: sum[1],
+				dob: sum[2],
+				sex: sum[3],
+				docs: sum[4],
+				docsD: sum[5]
+			}
+		)
+	}
+	summary()
+	// console.log(patient.name)
 	
 	// console.log(summary);
 	// const  {
@@ -27,7 +48,6 @@ const PatientScreen = ({ match }) => {
 
 	return (
 		<div>
-			{summary}
 			<Nav name={patient.name} />
 			<section id='patient'>
 				<Container>
@@ -38,20 +58,20 @@ const PatientScreen = ({ match }) => {
 								<Col lg={4}>{patient.name}</Col>
 							</Row>
 							<Row>
-								<Col lg={4}>email</Col>
-								<Col lg={4}>{patient.email}</Col>
-							</Row>
-							<Row>
 								<Col lg={4}>DATE OF BIRTH</Col>
 								<Col lg={4}>{patient.dob}</Col>
 							</Row>
 							<Row>
 								<Col lg={4}>SEX</Col>
-								<Col lg={4}>{patient.email}</Col>
+								<Col lg={4}>{patient.sex}</Col>
 							</Row>
 							<Row>
 								<Col lg={4}>aadhar</Col>
 								<Col lg={4}>{patient.aadhar}</Col>
+							</Row>
+							<Row>
+								<Col lg={4}>Address</Col>
+								<Col lg={4}>{patient.address}</Col>
 							</Row>
 						</Col>
 						<Col>
